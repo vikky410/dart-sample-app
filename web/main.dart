a807838909eb40527beafcfd981b1a20e0732609
vikky410 <vikky4fnds@gmail.com>
2017-02-16 10:03:47 +0000

    Navigating through the sample app

--- web/main.dart
+++ web/main.dart
@@ -5,4 +5,33 @@ import 'dart:html';
 
 void main() {
   querySelector('#output').text = 'Your Dart app is running.';
+
+  querySelector('#enter').onClick.listen(clickedOnEnter);
+}
+
+clickedOnEnter(e) {
+  InputElement input = querySelector("#commands");
+  input.style.borderColor = "green";
+
+  String output = "";
+
+  switch(input.value) {
+    case 'left':
+      output = "You are going left now";
+      break;
+    case 'right':
+      output = "You are going right now";
+      break;
+    case 'up':
+      output = "You are going up now";
+      break;
+    case 'down':
+      output = "You are going down now";
+      break;
+    default:
+      output = "Use left, right, up and down ...";
+  }
+
+  var outputHtml = "<div>${output}</div>";
+  querySelector("#output").appendHtml(outputHtml);
 }
